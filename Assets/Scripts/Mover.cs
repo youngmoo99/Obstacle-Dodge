a57564c65a99bb8ec4910cb375bb5a199772705a
YoungMoo Kim <rladuan612@gmail.com>
2025-06-04 21:24:31 +0900

    update colider & isTrigger

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,13 +2,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {   
-    [SerializeField]
-    float xValue = 0.01f;
-    [SerializeField]
-    float yValue = 0f;
-
-    [SerializeField]
-    float zValue = 0f;
+    [SerializeField] float moveSpeed = 8f;
 
 
     void Start()
@@ -17,7 +11,11 @@ public class Mover : MonoBehaviour
     }
 
     void Update()
-    {
+    {   
+        float xValue =  Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; //키보드 입력 접근 수평(Horizontal) 혹은 수직(Vertical) --> 좌,우
+        float yValue =  0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed; // 위, 아래
+        //일관되게 움직이기 위해 Time.deltaTime 사용 
         transform.Translate(xValue, yValue, zValue); // Translate(x,y,z) 현재 좌표에서 좌표만큼 이동
     }
 }
